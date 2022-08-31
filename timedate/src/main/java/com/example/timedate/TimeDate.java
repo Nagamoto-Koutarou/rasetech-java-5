253c885f4d4ecd78b0f43d9a2e8ca8d3b9441969
nagamoto <koutaro.n.1056@gmail.com>
2022-08-31 22:41:36 +0900

    Hello, World! に加えて、現在時刻を画面に表示するコードの追加。

--- timedate/src/main/java/com/example/timedate/TimeDate.java
+++ timedate/src/main/java/com/example/timedate/TimeDate.java
@@ -4,12 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class TimeDate {
 
     @GetMapping("/hello")
     public String hello(Model model) {
-        model.addAttribute("tag", "Hello, World!");
+
+        LocalDateTime nowDate = LocalDateTime.now();
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒 E曜日");
+        String formatNowDate = dtf2.format(nowDate);
+
+        model.addAttribute("tag", "TimeDate, World!");
+        model.addAttribute("timeDate", formatNowDate);
+
         return "index";
     }
 
